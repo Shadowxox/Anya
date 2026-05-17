b0237d99ffbbd2aa84afa9ed7d9c2d46e7dfe9d2
Shadowxox <shadowx0x12@gmail.com>
2026-05-18 01:19:20 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -2,20 +2,28 @@ from pyrogram.types import InlineKeyboardButton
 import config
 from ShrutiMusic import app
 
+
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_GROUP
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["E_X_1"], url=config.UPSTREAM_REPO),
-            InlineKeyboardButton(text=_["S_B_11"], callback_data="about_page")
+            InlineKeyboardButton(
+                text=_["S_B_11"],
+                callback_data="about_page"
+            )
         ],
     ]
     return buttons
+
 
 def private_panel(_):
     buttons = [
@@ -33,25 +41,37 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(
-                text=_["E_X_1"],
-                callback_data="fork_repo"
+                text=_["S_B_5"],
+                user_id=config.OWNER_ID
             ),
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="help_page_1")
+            InlineKeyboardButton(
+                text=_["S_B_4"],
+                callback_data="help_page_1"
+            )
         ],
     ]
     return buttons
 
+
 def about_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_GROUP
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
+            InlineKeyboardButton(
+                text=_["BACK_BUTTON"],
+                callback_data="settingsback_helper"
+            )
         ]
     ]
     return buttons
