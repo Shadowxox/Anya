73ff756afcdb339e2a02e295fceb9de64ce28791
Shadowxox <shadowx0x12@gmail.com>
2026-05-18 00:27:24 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -1,24 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
 from pyrogram.types import InlineKeyboardButton
 import config
 from ShrutiMusic import app
@@ -33,7 +12,7 @@ def start_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["E_X_1"], url=config.UPSTREAM_REPO),
-            InlineKeyboardButton(text=_["S_B_11"], callback_data="about_page")  # About button
+            InlineKeyboardButton(text=_["S_B_11"], callback_data="about_page")
         ],
     ]
     return buttons
@@ -50,10 +29,6 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_11"],
                 callback_data="about_page"
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_12"],
-                callback_data="owner_page"
             )
         ],
         [
@@ -80,31 +55,3 @@ def about_panel(_):
         ]
     ]
     return buttons
-
-def owner_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(text=_["S_H_1"], url=config.INSTAGRAM),
-            InlineKeyboardButton(text=_["S_H_2"], url=config.YOUTUBE),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_H_3"], url=config.GITHUB),
-            InlineKeyboardButton(text=_["S_H_4"], url=config.DONATE),
-        ],
-        [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
-        ]
-    ]
-    return buttons
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
