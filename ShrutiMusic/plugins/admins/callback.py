501b206bdcd2deefc86a4919c9c6953dd2d216fd
Shadowxox <shadowx0x12@gmail.com>
2026-05-18 01:20:57 +0530

    Update callback.py

--- ShrutiMusic/plugins/admins/callback.py
+++ ShrutiMusic/plugins/admins/callback.py
@@ -75,7 +75,7 @@ async def show_help_page1(client, callback_query: CallbackQuery):
         reply_markup=help_pannel_page1(_, START=True)
     )
 
-@app.on_callback_query(filters.regex("fork_repo"))
+@app.on_callback_query(filters.regex("fork_repooooooooooo"))
 async def fork_repo_callback(client, query):
     await query.message.edit_text(
         text=(
