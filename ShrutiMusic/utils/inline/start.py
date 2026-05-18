4c5a9f99d6266558e8a0b581a8df9c9454ddda3e
Shadowxox <shadowx0x12@gmail.com>
2026-05-18 16:17:39 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -9,11 +9,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_2"],
-                url=config.SUPPORT_GROUP
-            ),
+            )
         ],
         [
             InlineKeyboardButton(
@@ -21,9 +17,15 @@ def start_panel(_):
                 callback_data="about_page"
             ),
             InlineKeyboardButton(
+                text=_["S_B_5"],
+                user_id=config.OWNER_ID
+            ),
+        ],
+        [
+            InlineKeyboardButton(
                 text=_["S_B_4"],
                 callback_data="help_page_1"
-            ),
+            )
         ],
     ]
     return buttons
@@ -35,21 +37,23 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-            ),
+            )
+        ],
+        [
             InlineKeyboardButton(
                 text=_["S_B_11"],
                 callback_data="about_page"
             ),
-        ],
-        [
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 user_id=config.OWNER_ID
             ),
+        ],
+        [
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 callback_data="help_page_1"
-            ),
+            )
         ],
     ]
     return buttons
