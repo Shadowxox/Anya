b765122eb32a35ed3febff804dcbcdd79aaa1bcd
Shadowxox <shadowx0x12@gmail.com>
2026-05-21 01:44:13 +0530

    Update start.py

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -50,6 +50,9 @@ async def start_pm(client, message: Message, _):
     except:
         pass
 
+    user = message.from_user  # ✅ for logging
+
+    # ---------------- HELP ----------------
     if len(message.text.split()) > 1:
 
         name = message.text.split(None, 1)[1]
@@ -67,7 +70,6 @@ async def start_pm(client, message: Message, _):
                     has_spoiler=True,
                     message_effect_id=EFFECT_ID,
                 )
-
             except:
                 return await message.reply_video(
                     video=config.START_IMG_URL,
@@ -77,38 +79,29 @@ async def start_pm(client, message: Message, _):
                     has_spoiler=True,
                 )
 
+        # ---------------- SUDO ----------------
         if name[0:3] == "sud":
 
-            await sudoers_list(
-                client=client,
-                message=message,
-                _=_
-            )
+            await sudoers_list(client=client, message=message, _=_)
 
             if await is_on_off(2):
-
                 return await app.send_message(
                     chat_id=config.LOG_GROUP_ID,
                     text=f"{message.from_user.mention} started the bot to check sudo list.",
                 )
-
             return
 
+        # ---------------- INFO ----------------
         if name[0:3] == "inf":
 
             m = await message.reply_text("🔎")
 
             query = (str(name)).replace("info_", "", 1)
-
             query = f"https://www.youtube.com/watch?v={query}"
 
-            results = VideosSearch(
-                query,
-                limit=1
-            )
+            results = VideosSearch(query, limit=1)
 
             for result in (await results.next())["result"]:
-
                 title = result["title"]
                 duration = result["duration"]
                 views = result["viewCount"]["short"]
@@ -130,15 +123,8 @@ async def start_pm(client, message: Message, _):
             key = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(
-                            text=_["S_B_8"],
-                            url=link
-                        ),
-
-                        InlineKeyboardButton(
-                            text=_["S_B_9"],
-                            url=config.SUPPORT_GROUP
-                        ),
+                        InlineKeyboardButton(text=_["S_B_8"], url=link),
+                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_GROUP),
                     ],
                 ]
             )
@@ -155,7 +141,6 @@ async def start_pm(client, message: Message, _):
                     has_spoiler=True,
                     message_effect_id=EFFECT_ID,
                 )
-
             except:
                 await app.send_video(
                     chat_id=message.chat.id,
@@ -168,55 +153,13 @@ async def start_pm(client, message: Message, _):
 
             return
 
+        # ---------------- START CMD ----------------
         if name == "start":
 
             out = private_panel(_)
-
             UP, CPU, RAM, DISK = await bot_sys_stats()
 
-            try:
-                await message.reply_video(
-                    video=config.START_IMG_URL,
-                    caption=_["start_2"].format(
-                        message.from_user.mention,
-                        app.mention,
-                        UP,
-                        DISK,
-                        CPU,
-                        RAM,
-                    ),
-                    reply_markup=InlineKeyboardMarkup(out),
-                    supports_streaming=True,
-                    has_spoiler=True,
-                    message_effect_id=EFFECT_ID,
-                )
-
-            except:
-                await message.reply_video(
-                    video=config.START_IMG_URL,
-                    caption=_["start_2"].format(
-                        message.from_user.mention,
-                        app.mention,
-                        UP,
-                        DISK,
-                        CPU,
-                        RAM,
-                    ),
-                    reply_markup=InlineKeyboardMarkup(out),
-                    supports_streaming=True,
-                    has_spoiler=True,
-                )
-
-            return
-
-    else:
-
-        out = private_panel(_)
-
-        UP, CPU, RAM, DISK = await bot_sys_stats()
-
-        try:
-            await message.reply_video(
+            msg = await message.reply_video(
                 video=config.START_IMG_URL,
                 caption=_["start_2"].format(
                     message.from_user.mention,
@@ -232,25 +175,54 @@ async def start_pm(client, message: Message, _):
                 message_effect_id=EFFECT_ID,
             )
 
-        except:
-            await message.reply_video(
-                video=config.START_IMG_URL,
-                caption=_["start_2"].format(
-                    message.from_user.mention,
-                    app.mention,
-                    UP,
-                    DISK,
-                    CPU,
-                    RAM,
-                ),
-                reply_markup=InlineKeyboardMarkup(out),
-                supports_streaming=True,
-                has_spoiler=True,
-            )
+    else:
 
-        return
+        out = private_panel(_)
+        UP, CPU, RAM, DISK = await bot_sys_stats()
+
+        msg = await message.reply_video(
+            video=config.START_IMG_URL,
+            caption=_["start_2"].format(
+                message.from_user.mention,
+                app.mention,
+                UP,
+                DISK,
+                CPU,
+                RAM,
+            ),
+            reply_markup=InlineKeyboardMarkup(out),
+            supports_streaming=True,
+            has_spoiler=True,
+            message_effect_id=EFFECT_ID,
+        )
+
+    # ---------------- LOG START EVENT (FIXED) ----------------
+    try:
+        username = f"@{user.username}" if user.username else "Not Set"
+
+        log_text = f"""
+<blockquote>🎧 𝙉𝙀𝙒 𝙎𝙏𝘼𝙍𝙏 𝙀𝙑𝙀𝙉𝙏</blockquote>
+
+<blockquote>✨ User: {user.mention}</blockquote>
+<blockquote>🪪 Name: {user.first_name}</blockquote>
+<blockquote>👤 Username: {username}</blockquote>
+<blockquote>🆔 User ID: {user.id}</blockquote>
+
+<blockquote>🎶 Bot: {app.mention}</blockquote>
+<blockquote>💫 Action: Started bot in DM</blockquote>
+"""
+
+        await app.send_message(
+            chat_id=config.LOG_GROUP_ID,
+            text=log_text,
+            disable_web_page_preview=True,
+        )
+
+    except Exception as e:
+        print(f"LOG ERROR: {e}")
 
 
+# ---------------- GROUP START ----------------
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
@@ -276,7 +248,6 @@ async def start_gp(client, message: Message, _):
             has_spoiler=True,
             message_effect_id=EFFECT_ID,
         )
-
     except:
         await message.reply_video(
             video=config.START_IMG_URL,
@@ -289,47 +260,32 @@ async def start_gp(client, message: Message, _):
             has_spoiler=True,
         )
 
-    return await add_served_chat(
-        message.chat.id
-    )
+    return await add_served_chat(message.chat.id)
 
 
+# ---------------- BOT ADDED IN GROUP ----------------
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
 
     for member in message.new_chat_members:
 
         try:
-
-            language = await get_lang(
-                message.chat.id
-            )
-
+            language = await get_lang(message.chat.id)
             _ = get_string(language)
 
             if await is_banned_user(member.id):
-
                 try:
-                    await message.chat.ban_member(
-                        member.id
-                    )
+                    await message.chat.ban_member(member.id)
                 except:
                     pass
 
             if member.id == app.id:
 
                 if message.chat.type != ChatType.SUPERGROUP:
-
-                    await message.reply_text(
-                        _["start_4"]
-                    )
-
-                    return await app.leave_chat(
-                        message.chat.id
-                    )
+                    await message.reply_text(_["start_4"])
+                    return await app.leave_chat(message.chat.id)
 
                 if message.chat.id in await blacklisted_chats():
-
                     await message.reply_text(
                         _["start_5"].format(
                             app.mention,
@@ -338,10 +294,7 @@ async def welcome(client, message: Message):
                         ),
                         disable_web_page_preview=True,
                     )
-
-                    return await app.leave_chat(
-                        message.chat.id
-                    )
+                    return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
 
@@ -359,7 +312,6 @@ async def welcome(client, message: Message):
                         has_spoiler=True,
                         message_effect_id=EFFECT_ID,
                     )
-
                 except:
                     await message.reply_video(
                         video=config.START_IMG_URL,
@@ -374,10 +326,7 @@ async def welcome(client, message: Message):
                         has_spoiler=True,
                     )
 
-                await add_served_chat(
-                    message.chat.id
-                )
-
+                await add_served_chat(message.chat.id)
                 await message.stop_propagation()
 
         except Exception as ex:
